40d652e694c5383d733a5769d863095ec37b0ece
zhangfuwen <zhangfuwen@foxmail.com>
2026-07-07 16:19:07 +0800

    fix: export submodules in __init__.py so 'from tux_wallpaper import commons' works

--- tux_wallpaper/__init__.py
+++ tux_wallpaper/__init__.py
@@ -1,3 +1,12 @@
 """Tux Wallpaper - Video wallpaper player for Linux."""
 
 __version__ = "0.1.0"
+
+# Submodules
+from . import commons
+from . import config
+from . import fade
+from . import utils
+from .player import base_player
+from .player import handlers
+from .player import video_player
